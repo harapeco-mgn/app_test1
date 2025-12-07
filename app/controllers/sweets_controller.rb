7d0d8d62c60327d092851a458fb9da70bebbfc9e
harapeco-mgn <harapeco.mgn@gmail.com>
2025-12-07 19:24:32 +0900

    rubocopの修正

--- app/controllers/sweets_controller.rb
+++ app/controllers/sweets_controller.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class SweetsController < ApplicationController
-   def index
-    @sweet = Sweet.order("RANDOM()").first  # ランダムに1件取得
+  def index
+    @sweet = Sweet.order('RANDOM()').first # ランダムに1件取得
     # または特定のIDで取得する場合
     # @sweet = Sweet.find(params[:id])
   end
+
   def show; end
 
   def new; end
