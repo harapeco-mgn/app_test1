aacdf79b62ae956c06956065127dcb13d64a32a9
harapeco-mgn <harapeco.mgn@gmail.com>
2025-12-10 14:44:43 +0900

    トップのCSS

--- app/controllers/sweets_controller.rb
+++ app/controllers/sweets_controller.rb
@@ -5,14 +5,4 @@ class SweetsController < ApplicationController
     @sweet = Sweet.order('RANDOM()').first
   end
 
-  def show; end
-
-  def new; end
-
-  def edit; end
-  def create; end
-
-  def update; end
-
-  def destroy; end
 end
